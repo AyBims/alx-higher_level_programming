9b4e9738fa8c3da76be0d022af03bd089cb07db9
Joy-learns <adeshinaayomide12@yahoo.com>
2022-06-20 23:37:11 +0000

    indentations

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,5 +9,5 @@ def safe_print_list(my_list=[], x=0):
             info += 1
         except IndexError:
             break
-        print("")
-        return (info)
+    print("")
+    return (info)
