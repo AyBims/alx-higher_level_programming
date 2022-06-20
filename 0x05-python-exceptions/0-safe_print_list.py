d132ce25050444dd44b079e6a6c3d0c91f5ab50c
Joy-learns <adeshinaayomide12@yahoo.com>
2022-06-20 23:21:33 +0000

    handling exceptions

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    info = 0
+    for i in range(x):
+        try:
+            print("{}", .format(mylist[i]), end="")
+            info += 1
+        except IndexError:
+            break
+        print("")
+        return (info)
